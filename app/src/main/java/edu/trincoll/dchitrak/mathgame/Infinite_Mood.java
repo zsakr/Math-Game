4146ca0542883afccb0e58c84e9d77697b395a45
ziad Sakr <ziad.sakr@trincoll.edu>
2019-10-26 13:39:32 -0400

    Updated black interface

---
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/Infinite_Mood.java
@@ -0,0 +1,14 @@
+package edu.trincoll.dchitrak.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Infinite_Mood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infinite__mood);
+    }
+}
