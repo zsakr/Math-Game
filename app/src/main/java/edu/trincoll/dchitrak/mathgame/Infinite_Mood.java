89aa440dd4b08ddd62b3b1b2459caba743bdfc3a
Bettina <bettina.kingsmith@gmail.com>
2019-10-26 13:41:37 -0400

    Update Infinite_Mood.java

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/Infinite_Mood.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/Infinite_Mood.java
@@ -9,6 +9,6 @@ public class Infinite_Mood extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_infinite__mood);
+        setContentView(R.layout.activity_infinite_mode);
     }
 }
