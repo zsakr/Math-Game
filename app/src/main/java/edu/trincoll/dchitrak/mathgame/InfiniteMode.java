3a678e1d90f6a52e756920bf5e0ae2e2db0c6daf
Bettina <bettina.kingsmith@gmail.com>
2019-10-26 13:42:25 -0400

    renamed infinite mode class file

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/InfiniteMode.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/InfiniteMode.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Infinite_Mood extends AppCompatActivity {
+public class InfiniteMode extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
