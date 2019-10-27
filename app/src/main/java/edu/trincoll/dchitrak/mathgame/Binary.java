d649e7538ef6eb92973b48ab0fa3933b30076bc9
DigeshC <chitrakar.digesh@gmail.com>
2019-10-27 02:30:59 -0400

    Stuff

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/Binary.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/Binary.java
@@ -5,4 +5,19 @@ public class Binary extends GenerateProblem {
     public Binary(int min, int max) {
         super(min, max);
     }
+
+    public void makeProblem(){
+        super.makeProblem();
+        if(op.equals("*")){
+            op = "+";
+        } else{
+            op = "-"
+        }
+
+        convert();
+    }
+
+    public void convert(){
+        num1 =
+    }
 }
