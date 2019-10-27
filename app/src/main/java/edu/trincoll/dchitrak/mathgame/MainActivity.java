47dc1badfb27c6b0b6b2b585f34f28734cb6a4ad
DigeshC <chitrakar.digesh@gmail.com>
2019-10-27 00:00:18 -0400

    Added rocket

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/MainActivity.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/MainActivity.java
@@ -2,18 +2,19 @@ package edu.trincoll.dchitrak.mathgame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    boolean toggle = true;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    private void menuSelection(){
         /* this button is for the fixed game setup */
         Button press = (Button) findViewById(R.id.button);
         press.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +49,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startint);
             }
         });
-
-
     }
-}
+
+    private void rocketAnimate() {
+
+//        TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, -2000.0f);          //  new TranslateAnimation(xFrom,xTo, yFrom,yTo)
+//        animation.setDuration(10000);  // animation duration
+//        //animation.setRepeatCount(5);  // animation repeat count
+//        //animation.setFillAfter(true);
+//
+//        /* this button is for the infinte mode setup */
+        Button rocketPress = (Button) findViewById(R.id.blastButt);
+        rocketPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (toggle){
+                ImageView animate = findViewById(R.id.rocket);
+                animate.setImageResource(R.drawable.bomb_transparent);
+                toggle = !toggle;
+            }else{
+                    ImageView animate = findViewById(R.id.rocket);
+                    animate.setImageResource(R.drawable.rocket);
+                    toggle = !toggle;
+                }
+            }
+        });
+
+
+//        if (move) {
+//            animate.startAnimation(animation);  // start animation
+//
+//        }
+//
+//        if (!move) {
+//
+//        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        menuSelection();
+        rocketAnimate();
+    }}
+
