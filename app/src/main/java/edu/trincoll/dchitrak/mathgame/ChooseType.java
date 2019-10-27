0cfd07125e793fb24ac70ef3804668a88a2b59af
Bettina <bettina.kingsmith@gmail.com>
2019-10-27 00:50:04 -0400

    for Ziad, init page for choosing binary/dec/hex

---
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/ChooseType.java
@@ -0,0 +1,14 @@
+package edu.trincoll.dchitrak.mathgame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_type);
+    }
+}
