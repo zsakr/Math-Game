0706593c9a5e338f8da7666b64117bcaf2e6dcb9
Bettina <bettina.kingsmith@gmail.com>
2019-10-26 23:50:41 -0400

    minor changes to buttons

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/FixedGame.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/FixedGame.java
@@ -3,7 +3,6 @@ package edu.trincoll.dchitrak.mathgame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- app/src/main/java/edu/trincoll/dchitrak/mathgame/ResultsPage.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/ResultsPage.java
@@ -5,15 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ResultsPage extends AppCompatActivity {
 
     private void buttonClick(){
-        ImageButton exitButton = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton exitButton = (ImageButton) findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
